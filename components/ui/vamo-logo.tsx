c46a0c0ab39831be57fe93914635a0636af6245b
[dyad] <git@dyad.sh>
2025-10-18 14:30:58 -0300

    [dyad] Corregir error de renderizado del logo SVG - wrote 1 file(s) + extra files edited outside of Dyad

--- components/ui/vamo-logo.tsx
+++ components/ui/vamo-logo.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import VamoSvg from '@/assets/images/VAMO.svg';
+import VamoSvg from '../../assets/images/VAMO.svg';
 
 export function VamoLogo({ width = 100, height = 40 }: { width?: number; height?: number }) {
   return <VamoSvg width={width} height={height} />;
