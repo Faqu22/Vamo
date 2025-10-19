4d8a9f656117ef574afefd9ee71d7684a5a87979
Faqu22 <facundoalvarez415@gmail.com>
2025-10-18 21:03:25 -0300

    fix deprecated

--- app/edit-profile.tsx
+++ app/edit-profile.tsx
@@ -78,7 +78,7 @@ export default function EditProfileScreen() {
     }
 
     const result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      mediaTypes: ['images'],
       allowsEditing: true,
       aspect: [1, 1],
       quality: 1,
